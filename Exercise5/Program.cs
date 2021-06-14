11c946bf02a7244191f71941324b11569c2b7f60
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-14 07:08:13 -0700

    Add files via upload

---
+++ Exercise5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+       
+
+
+            var customers = CreateCustomers();
+            var customerDictionary = new Dictionary<Customer, string>();
+
+            foreach (var c in customers)
+                customerDictionary.Add(c, c.Name.Split(' ')[1]);
+
+            var matches = customerDictionary.FilterBy((customer, lastName) => lastName.StartsWith("A"));
+           
+            Console.WriteLine("Number of Matches: {0}", matches.Count);
+
+
+        }
+        public static List<Customer> FindCustomersByCity(List<Customer> customers, string city)
+        {
+            return customers.FindAll(c => c.City == city);
+        }
+
+        static List<Customer> CreateCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer { Name = "jessy", City = "LA" },
+                new Customer { Name = "tom", City = "new york" }
+            };
+        }
+    }
+
+    class Customer
+    {
+        public String Name { get; set; }
+        public String City { get; set; }
+    }
+}
---
+++ Exercise5/extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    public delegate bool KeyValueFilter<K, V>(K key, V value);
+    public static class Extensions
+    {
+        public static List<K> FilterBy<K, V>(this Dictionary<K, V> items, KeyValueFilter<K, V> filter)
+        {
+            var result = new List<K>();
+
+            foreach (KeyValuePair<K, V> element in items)
+            {
+                if (filter(element.Key, element.Value))
+                    result.Add(element.Key);
+            }
+            return result;
+        }
+
+    }
+}
