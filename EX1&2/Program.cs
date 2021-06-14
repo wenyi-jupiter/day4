9057a8f74c73c600718b8e96e6c6a9e64ee55099
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-14 07:04:54 -0700

    Add files via upload

---
+++ EX1&2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4
+{
+    public class customer
+    {
+        public customer(int id)
+        {
+            CID = id;
+        }
+        public int CID { get;private set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return Name + "\t" + City;
+        }
+
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //customer c = new customer(1) ;
+            //c.Name = "Maria"; 
+            //c.City = "Berlin";
+            customer c= new customer(1) { Name = "Jack", City = "Washington" };
+            //c.CID = 1;
+            Console.WriteLine(c);
+            List<point> square = new List<point>
+            {
+                new point{X=0,Y=5 },
+                new point{X=5,Y=5}
+            };
+
+            //List<customer> c1 = createc();
+            Console.WriteLine("customers:\n");
+            foreach(customer c2 in createc())
+            {
+                Console.WriteLine(c2);
+            }
+
+
+        }
+
+        static List<customer> createc()
+        {
+            return new List<customer>
+            {
+                new customer(1){Name="sally",City="london"},
+                new customer(2){Name="sallh",City="liverpool"}
+            };
+        }
+    }
+}
---
+++ EX1&2/point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    class point
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+
+    }
+}
