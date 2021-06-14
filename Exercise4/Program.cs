c1d6ad34722d17bad59899562daae98a8841c428
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-14 07:07:35 -0700

    Add files via upload

---
+++ Exercise4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var newc = new customer(10)
+            {
+                Name = "roel",
+                City = "madrid"
+
+            };
+            foreach(var i in createc())
+            {
+                if (extensions.comapre(newc, i))
+                {
+                    Console.WriteLine("the new customer was already in the list");
+                }
+            }
+            Console.WriteLine("the new customer was not in the list");
+            var cs = createc();
+
+            var addedCustomers = new List<customer>
+            {
+                 new customer(9)  { Name = "frank", City = "irvine" },
+                 new customer(10) { Name = "Diego", City = "manchester" }
+
+            };
+            var updatedCustomers = cs.Append(addedCustomers);
+
+            foreach (var c in updatedCustomers)
+            {
+                if (newc.comapre(c))
+                {
+                    Console.WriteLine("The new customer was already in the list");
+                    return;
+                }
+
+            }
+            Console.WriteLine("The new customer was not in the list");
+
+        }
+        static List<customer> createc()
+        {
+            return new List<customer>
+            {
+                new customer(1){Name="sally",City="london"},
+                new customer(2){Name="sallh",City="liverpool"}
+            };
+        }
+    }
+}
---
+++ Exercise4/extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise4
+{
+    public static class extensions
+    {
+        public static bool comapre(this customer c1, customer c2)
+        {
+            if(c1.CID==c2.CID && c1.Name==c2.Name && c1.City == c2.City)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static List<T> Append<T>(this List<T> a, List<T> b)
+        {
+            var newList = new List<T>(a);
+            newList.AddRange(b);
+            return newList;
+        }
+
+    }
+    public class customer
+    {
+        public customer(int id)
+        {
+            CID = id;
+        }
+        public int CID { get; private set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return Name + "\t" + City;
+        }
+ 
+
+
+    }
+}
